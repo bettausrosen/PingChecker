4ae1ef60b3da0d5ae92a0af8b275e8d43ffddc6e
BettAusRosen <87874857+bettausrosen@users.noreply.github.com>
2022-08-14 18:06:05 +0200

    Update PingCommand.java

--- src/main/java/de/bettausrosen/pingchecker/commands/PingCommand.java
+++ src/main/java/de/bettausrosen/pingchecker/commands/PingCommand.java
@@ -2,6 +2,7 @@ package de.bettausrosen.pingchecker.commands;
 
 import de.bettausrosen.pingchecker.PingChecker;
 import net.md_5.bungee.api.CommandSender;
+import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.plugin.Command;
 
@@ -13,25 +14,45 @@ public class PingCommand extends Command {
 
     @Override
     public void execute(CommandSender sender, String[] args) {
-        if(sender instanceof ProxiedPlayer) {
-            ProxiedPlayer player = (ProxiedPlayer)sender;
-            if(args.length == 0) {
-                if(player.getPing() <= 50) {
+        if (sender instanceof ProxiedPlayer) {
+            ProxiedPlayer player = (ProxiedPlayer) sender;
+            if (args.length == 0) {
+                if (player.getPing() <= 70) {
                     sender.sendMessage(PingChecker.PREFIX + "§7Your ping is §2§l" + player.getPing() + "§7!");
                 }
-                if(player.getPing() <= 100 && player.getPing() >= 50) {
+                if (player.getPing() <= 100 && player.getPing() > 70) {
                     sender.sendMessage(PingChecker.PREFIX + "§7Your ping is §a§l" + player.getPing() + "§7!");
                 }
-                if(player.getPing() >= 100 && player.getPing() <= 130) {
+                if (player.getPing() > 100 && player.getPing() <= 130) {
                     sender.sendMessage(PingChecker.PREFIX + "§7Your ping is §e§l" + player.getPing() + "§7!");
                 }
-                if(player.getPing() >= 130 && player.getPing() <= 180) {
+                if (player.getPing() > 130 && player.getPing() <= 180) {
                     sender.sendMessage(PingChecker.PREFIX + "§7Your ping is §6§l" + player.getPing() + "§7!");
                 }
-                if(player.getPing() >= 180) {
+                if (player.getPing() > 180) {
                     sender.sendMessage(PingChecker.PREFIX + "§7Your ping is §4§l" + player.getPing() + "§7!");
                 }
-            } else {
+            }
+            if (args.length == 1) {
+                ProxiedPlayer target = ProxyServer.getInstance().getPlayer(args[0]);
+                if (target.getPing() <= 70) {
+                    sender.sendMessage(PingChecker.PREFIX + "§e" + target.getDisplayName() +  "'s ping is §2§l" + target.getPing() + "§7!");
+                }
+                if (target.getPing() <= 100 && target.getPing() > 70) {
+                    sender.sendMessage(PingChecker.PREFIX + "§e" + target.getDisplayName() +  "'s ping is §a§l" + target.getPing() + "§7!");
+                }
+                if (target.getPing() > 100 && target.getPing() <= 130) {
+                    sender.sendMessage(PingChecker.PREFIX + "§e" + target.getDisplayName() +  "'s ping is §e§l" + target.getPing() + "§7!");
+                }
+                if (target.getPing() > 130 && target.getPing() <= 180) {
+                    sender.sendMessage(PingChecker.PREFIX + "§e" + target.getDisplayName() +  "'s ping is §6§l" + target.getPing() + "§7!");
+                }
+                if (target.getPing() > 180) {
+                    sender.sendMessage(PingChecker.PREFIX + "§e" + target.getDisplayName() +  "'s ping is §4§l" + target.getPing() + "§7!");
+                }
+            }
+
+            if (args.length == 2) {
                 sender.sendMessage(PingChecker.PREFIX + "§4§lSyntax: §c/ping");
             }
         } else {
